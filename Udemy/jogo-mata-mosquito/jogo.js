89873f71092bb2dcbc3323c2013896d141a8272f
BrunoViola <brunohenriquesilvaviola@gmail.com>
2025-05-28 20:32:40 -0300

    randomiza tamanho mosquito [jogo-mata-mosquito]

--- Udemy/jogo-mata-mosquito/jogo.js
+++ Udemy/jogo-mata-mosquito/jogo.js
@@ -7,6 +7,8 @@ function ajustaTamanhoPalcoJogo() {
    largura = window.innerWidth;
    console.log(largura, altura);
 }
+
+// posicionando o mosquito pelo palco do jogo
 function posicionaMosquito(){
    // randomiza as posições dos mosquitos
    var posicaoX = Math.floor(Math.random() * largura) - 90;
@@ -19,12 +21,26 @@ function posicionaMosquito(){
    // cria o elemento html
    var mosquito = document.createElement('img');
    mosquito.src = 'img/mosquito.png';
-   mosquito.className = 'mosquito-principal';
+   mosquito.className = tamanhoAleatorio();
    mosquito.style.left = posicaoX + 'px';
    mosquito.style.top = posicaoY + 'px';
    mosquito.style.position = 'absolute';
 
    document.body.appendChild(mosquito);
+}
+
+// randomiza o tamanho do mosquito
+function tamanhoAleatorio() {
+   var classe = Math.floor(Math.random()*3);
+
+   switch (classe) {
+      case 0:
+         return 'mosquito-grande'
+      case 1:
+         return 'mosquito-medio'
+      case 2:
+         return 'mosquito-pequeno'
+   }
 }
 
 function main() {
