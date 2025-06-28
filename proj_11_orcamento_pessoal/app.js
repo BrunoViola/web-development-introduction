d99acf5965d805224d6b43871ad02a32737a9296
BrunoViola <brunohenriquesilvaviola@gmail.com>
2025-06-28 12:34:26 -0300

    feat: índices dinâmicos para armazenamento de múltiplas despesas [proj_11_orcamento_pessoal]

--- proj_11_orcamento_pessoal/app.js
+++ proj_11_orcamento_pessoal/app.js
@@ -9,6 +9,30 @@ class Despesa {
    }
 }
 
+class Bd {
+   constructor(){
+      let id = localStorage.getItem('id');
+
+      if(id === null){
+         localStorage.setItem('id', 0);
+      }
+   }
+
+   getProximoId(){
+      let proximoId = localStorage.getItem('id');
+      proximoId = parseInt(proximoId) + 1;
+      localStorage.setItem('id', proximoId)
+      return proximoId;
+   }
+
+   gravar(despesa) {
+      let id = this.getProximoId();
+      localStorage.setItem(id, JSON.stringify(despesa));
+   }
+}
+
+let bd = new Bd();
+
 function cadastrarDespesa(){
    let ano = document.getElementById('ano');
    let mes = document.getElementById('mes');
@@ -26,9 +50,5 @@ function cadastrarDespesa(){
       valor.value 
    );
 
-   gravar(despesa);
-}
-
-function gravar(despesa) {
-   localStorage.setItem('despesa', JSON.stringify(despesa));
+   bd.gravar(despesa);
 }
